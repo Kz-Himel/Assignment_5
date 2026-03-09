0cc45490927943888b7a21e97770196d89765a5e
Kz-Himel <kzhimel129@gmail.com>
2026-03-09 11:37:39 +0600

    Add tab functionality

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+let allIssues = [];
+
 const loadIssues = () => {
     const url = `https://phi-lab-server.vercel.app/api/v1/lab/issues`;
 
@@ -5,8 +7,33 @@ const loadIssues = () => {
     .then((res) => res.json())
     .then((json) => {
         // console.log(json.data);
-        showIssues(json.data);
-    })
+        allIssues = json.data;
+        showIssues(allIssues);
+    });
+}
+
+// Changing tab function
+const changeTab = (status) => {
+
+    // remove active class from all tabs
+    document.getElementById("tab-all").classList.remove("btn-primary");
+    document.getElementById("tab-open").classList.remove("btn-primary");
+    document.getElementById("tab-closed").classList.remove("btn-primary");
+
+    // add active class to selected tab
+    document.getElementById("tab-" + status).classList.add("btn-primary");
+
+    let filteredIssues = [];
+
+    if(status === "all"){
+        filteredIssues = allIssues;
+    }
+    else{
+        filteredIssues = allIssues.filter(issue => issue.status === status);
+    }
+
+    showIssues(filteredIssues);
+
 }
 
 // ShowIssues() --- For display all Issues
@@ -16,6 +43,9 @@ const showIssues = (issues) => {
     const issueContainer = document.getElementById("issue-container");
     // Empty the container
     issueContainer.innerHTML = "";
+
+    // For showing Isssue count dynamically
+    document.getElementById("issue-count").innerText = issues.length + " Issues";
 
     //2. Start a loop for getting issues
     issues.forEach(issue => {
@@ -29,19 +59,36 @@ const showIssues = (issues) => {
         // Condiotons for borderr color
         if(issue.status == "open") {
             borderColor = "border-green-500";
+            // status bases img
+            img = "assets/Open-Status.png"
         }
         else {
             borderColor = "border-purple-600";
+            // status bases img
+            img = "assets/Closed-Status.png"
         }
 
         card.className = `bg-white p-4 rounded shadow border-t-4 ${borderColor}`;
+
+        // Condition for priority and their color
+        let priorityColor = "";
+
+        if(issue.priority === "high"){
+            priorityColor = "bg-red-200 text-red-500";
+        }
+        else if(issue.priority === "medium"){
+            priorityColor = "bg-yellow-100 text-yellow-500";
+        }
+        else if(issue.priority === "low"){
+            priorityColor = "bg-gray-200 text-gray-500";
+        }
 
         // Innerhtml of the issue cards
         card.innerHTML = `
                 <div class="upper flex flex-col gap-3 p-4">
           <div class="flex justify-between">
-        <img class="h-[24px] w-auto" src="assets/Open-Status.png" alt="">
-        <p class="bg-red-200 font-bold uppercase px-5 py-1 text-red-500 rounded-[18px]">${issue.priority}</p>
+        <img class="h-[24px] w-auto" src="${img}" alt="">
+        <p class="${priorityColor} font-bold uppercase px-5 py-1 rounded-[18px]">${issue.priority}</p>
       </div>
 
       <div class="">
